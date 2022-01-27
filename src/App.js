d805e07fce97eacd2cdaf1d392b516e916582f16
Pradeep Kumar <pk6361439@gmail.com>
2022-01-27 10:11:25 +0530

    Add Functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+// Import required part
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [result,setResult]=useState("")
+  // Function for hnadle input
+  const input=(e)=>{
+    setResult(result.concat(e.target.value));
+    console.log(e.target.value)
+  }
+  // Function for handle reset button
+  const reset=()=>{
+    setResult("");
+  }
+  // Function for calculate value
+  const total_val=()=>{
+    console.log(result);
+    setResult(eval(result).toString())
+  }
+  // All html that show in webpage
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className='container'>
+    {/* Section for display result */}
+    <div className='screen'>
+      <input type='text' placeholder='0' className='input_box' value={result} />
+     </div>
+     {/* Create All button and call their related function with their value */}
+    <div className='buttons'>
+    <button className='btn' onClick={reset}>C</button>
+    <button className='btn' onClick={reset}>+/-</button>
+    <button className='btn' onClick={input} value='%'>%</button>
+    <button className='btn orange' onClick={input} value='/'>/</button>
+    <button className='btn' onClick={input} value='7'>7</button>
+    <button className='btn' onClick={input} value='8'>8</button>
+    <button className='btn' onClick={input} value='9'>9</button>
+    <button className='btn orange' onClick={input} value='*'>X</button>
+    <button className='btn' onClick={input} value='4'>4</button>
+    <button className='btn' onClick={input} value='5'>5</button>
+    <button className='btn' onClick={input} value='6'>6</button>
+    <button className='btn orange' onClick={input} value='-'>-</button>
+    <button className='btn' onClick={input} value='1'>1</button>
+    <button className='btn' onClick={input} value='2'>2</button>
+    <button className='btn' onClick={input} value='3'>3</button>
+    <button className='btn orange' onClick={input} value='+'>+</button>
+    <button className='btn zero' onClick={input} value='0'>0</button>
+    <button className='btn' onClick={input} value='.'>.</button>
+    <button className='btn orange' onClick={total_val}>=</button>
+    
+  </div>
+  </div>
   );
 }
 
